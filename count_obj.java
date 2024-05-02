18936c664fbafa880ac322016422ce1973c50f04
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-05-02 12:10:16 +0530

    commit

--- count_obj.java
+++ count_obj.java
@@ -1,19 +1,34 @@
-class Obj{
+class Obj {
     static int count = 0;
 
-    Obj(){
+    Obj() {
         count++;
     }
 }
 
 public class count_obj {
+    static int count = 0;
+
+    count_obj(){
+        count++;
+    }
+
     public static void main(String[] args) {
+        // To create objects of outer class (Obj)
         Obj Obj1 = new Obj();
         Obj Obj2 = new Obj();
         Obj Obj3 = new Obj();
         Obj Obj4 = new Obj();
         Obj Obj5 = new Obj();
+        System.out.println("No. of count_objects = " + Obj.count);
 
-        System.out.println("No. of objects = " + Obj.count);
+        // To create objects of samme class (count_obj)
+        count_obj count_obj1 = new count_obj();
+        count_obj count_obj2 = new count_obj();
+        count_obj count_obj3 = new count_obj();
+        count_obj count_obj4 = new count_obj();
+        count_obj count_obj5 = new count_obj();
+
+        System.out.println("No. of count_objects = " + count);
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+
+        Obj Obj2 = new Obj();
+        Obj Obj3 = new Obj();
+        Obj Obj4 = new Obj();
+        Obj Obj5 = new Obj();
+        System.out.println("No. of count_objects = " + 
