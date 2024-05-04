86a695dd27a8b6af4c80301b75bdf35926216fd1
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-05-04 18:55:28 +0530

    commit

---
+++ FrogClimb.java
@@ -0,0 +1,11 @@
+public class FrogClimb {
+
+    public static void main(String[] args) {
+        int height = 30;
+        int heightPerHour = 3-2;
+
+        float totalHour = height / heightPerHour;
+
+        System.out.println("Hours taken : " + totalHour);
+    }
+}
