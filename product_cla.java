4c688b069af79e58859225563bd8a7aed1b1f2dc
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-05-04 18:46:26 +0530

    commit

---
+++ product_cla.java
@@ -0,0 +1,9 @@
+public class product_cla {
+
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("Product : " + a*b);
+    }
+}
