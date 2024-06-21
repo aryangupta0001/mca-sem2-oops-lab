355b3ea09581dfb4e75ca2534124e3a1dd3fc7a0
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-06-22 03:06:12 +0530

    commit

--- practical_file/Constructors.java
+++ practical_file/Constructors.java
@@ -1,5 +1,38 @@
+class Animal
+{   String Name;
+    String Color;
+    
+    Animal()
+    {
+        System.out.println("This is an Animal\n");
+    }
+
+    Animal(String name, String color)
+    {
+        this.Name = name;
+        this.Color = color;
+
+        display();
+    }
+
+    Animal(Animal cat)
+    {
+        this.Name = cat.Name;
+        this.Color = cat.Color;
+
+        display();
+    }
+
+    void display()
+    {
+        System.out.println("Name of animal : " + Name + "\nColor of Animal : " + Color + "\n");
+    }
+}
+
 public class Constructors {
     public static void main(String[] args) {
-        
+        Animal dog = new Animal();
+        Animal cat1  = new Animal("Cat", "Grey");
+        Animal cat2 = new Animal(cat1);
     }
 }
